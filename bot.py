eb025b11d60f3e990427793e86b46cee2001e174
thereisnothinginhere <96543392+thereisnothinginhere@users.noreply.github.com>
2022-07-17 20:51:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '5191564607:AAFSZZbMbIauTP7VyPo9eQpCv6nQOkrJjx4'
+TOKEN = '5457888637:AAE_KUp0xUfYmgyApCOjQdkHsShAtx36wvQ'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -53,7 +53,7 @@ def main():
         listen="0.0.0.0",
         port=int(PORT),
         url_path=TOKEN,
-        webhook_url='https://yourherokuappname.herokuapp.com/' + TOKEN
+        webhook_url='https://fierce-springs-47777.herokuapp.com/' + TOKEN
     )
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
